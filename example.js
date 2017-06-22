4b00e562495a953169f96cd4b086276d21280fc9
coco <koko_16N1300059@nnn.ed.jp>
2017-06-22 11:12:05 +0900

    cronjob の記述

--- example.js
+++ example.js
@@ -9,9 +9,18 @@ const controller = Botkit.slackbot({
     debug: true
 });
 
-var bot = controller.spawn({
-  token: process.env.DRESS_TELLER_TOKEN_PRO
-}).startRTM();
+const bot = controller.spawn({
+    token: process.env.DRESS_TELLER_TOKEN_PRO
+}).startRTM(function(err, bot, playload){
+    if(err) throw new Error('Could not connect to Slack');
+    new CronJob({
+        // cronTime: '0 7 * * *', // 毎日朝の7時に
+        cronTime: '* * * * *',// とにかく毎分
+        onTick: tellTDress(),
+        start: true,
+        timeZone: 'Asia/Tokyo',
+    });
+});
 
 controller.hears(['hello'], 'direct_message,direct_mention,mention', function(bot, message) {
     bot.reply(message, 'hello!');
