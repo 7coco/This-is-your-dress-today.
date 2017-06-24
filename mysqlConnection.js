65c09a93c2ae3331960632ce3ffb4dc475ad3721
coco <koko_16N1300059@nnn.ed.jp>
2017-06-24 17:17:29 +0900

    database

--- mysqlConnection.js
+++ mysqlConnection.js
@@ -4,7 +4,7 @@ connection.configure({
     host: 'us-cdbr-iron-east-03.cleardb.net',
     user: 'bd4851ba8155c4',
     password: '9943b0e5',
-    database: 'dress_teller',
+    database: 'heroku_8621849c7281689',
     charset: 'utf8mb4',
 });
 
