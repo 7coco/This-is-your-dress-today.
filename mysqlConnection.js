9b299a8ce5747874d11fb87c8ec5c75818693594
coco <koko_16N1300059@nnn.ed.jp>
2017-06-24 17:13:41 +0900

    password

--- mysqlConnection.js
+++ mysqlConnection.js
@@ -3,6 +3,7 @@ const connection = require('mysql-promise')();
 connection.configure({
     host: 'us-cdbr-iron-east-03.cleardb.net',
     user: 'bd4851ba8155c4',
+    password: '9943b0e5',
     database: 'dress_teller',
     charset: 'utf8mb4',
 });
