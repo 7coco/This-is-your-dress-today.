200076d181dd1337524051ac417ff097ba8a06f3
coco <koko_16N1300059@nnn.ed.jp>
2017-06-24 17:11:15 +0900

    mysql

--- mysqlConnection.js
+++ mysqlConnection.js
@@ -1,8 +1,8 @@
 const connection = require('mysql-promise')();
 
 connection.configure({
-    host: 'localhost',
-    user: 'root',
+    host: 'us-cdbr-iron-east-03.cleardb.net',
+    user: 'bd4851ba8155c4',
     database: 'dress_teller',
     charset: 'utf8mb4',
 });
