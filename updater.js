a65910d546cf285dbef9a4c40abc6e253f86deba
7coco <coco.greenery@gmail.com>
2018-06-07 16:24:57 +0900

    スネークケースだった変数を直した

--- updater.js
+++ updater.js
@@ -12,14 +12,14 @@ class Updater {
         conn.query(query, [today, dressId]);
     }
 
-    updateLastWearedAt(outer_id, under_id){
+    updateLastWearedAt(outerId, underId){
         var time = new Time();
         var today = time.getToday();
         var conn = this.conn;
         var outerQuery = "UPDATE `outerwears` SET `last_weared_at` = ? WHERE `id` = ?";
         var underQuery = "UPDATE `underwears` SET `last_weared_at` = ? WHERE `id` = ?";
-        conn.query(outerQuery, [today, outer_id]);
-        conn.query(underQuery, [today, under_id]);
+        conn.query(outerQuery, [today, outerId]);
+        conn.query(underQuery, [today, under]);
     }
 }
 
